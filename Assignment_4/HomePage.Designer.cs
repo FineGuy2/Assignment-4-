9c1bc565f364399847e421b2a05dd82c25bb6a6e
Eyuel Solomon <110692570+FineGuy2@users.noreply.github.com>
2022-08-07 13:58:21 +0300

    Form Created

---
+++ Assignment_4/HomePage.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace Assignment_4
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNum = new System.Windows.Forms.Label();
+            this.lblInvNum = new System.Windows.Forms.Label();
+            this.lblObjName = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblNothing = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(34, 44);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(44, 13);
+            this.lblNum.TabIndex = 0;
+            this.lblNum.Text = "Number";
+            // 
+            // lblInvNum
+            // 
+            this.lblInvNum.AutoSize = true;
+            this.lblInvNum.Location = new System.Drawing.Point(265, 44);
+            this.lblInvNum.Name = "lblInvNum";
+            this.lblInvNum.Size = new System.Drawing.Size(91, 13);
+            this.lblInvNum.TabIndex = 1;
+            this.lblInvNum.Text = "Inventory Number";
+            // 
+            // lblObjName
+            // 
+            this.lblObjName.AutoSize = true;
+            this.lblObjName.Location = new System.Drawing.Point(34, 84);
+            this.lblObjName.Name = "lblObjName";
+            this.lblObjName.Size = new System.Drawing.Size(69, 13);
+            this.lblObjName.TabIndex = 2;
+            this.lblObjName.Text = "Object Name";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(34, 127);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(34, 13);
+            this.lblPrice.TabIndex = 3;
+            this.lblPrice.Text = "Price ";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(265, 127);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(35, 13);
+            this.lblCount.TabIndex = 4;
+            this.lblCount.Text = "Count";
+            // 
+            // lblNothing
+            // 
+            this.lblNothing.AutoSize = true;
+            this.lblNothing.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNothing.Location = new System.Drawing.Point(92, -7);
+            this.lblNothing.Name = "lblNothing";
+            this.lblNothing.Size = new System.Drawing.Size(264, 34);
+            this.lblNothing.TabIndex = 5;
+            this.lblNothing.Text = "Please Fill the following Form ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(392, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(120, 81);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(236, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(98, 124);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(336, 124);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.AllowDrop = true;
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.DarkGray;
+            this.dateTimePicker1.Location = new System.Drawing.Point(438, 78);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(378, 84);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 12;
+            this.lblDate.Text = "Date";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.DarkGray;
+            this.btnAdd.Location = new System.Drawing.Point(123, 202);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add Product";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.DarkGray;
+            this.btnClear.Location = new System.Drawing.Point(281, 202);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear ";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.DarkGray;
+            this.btnCancel.Location = new System.Drawing.Point(438, 202);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblNothing);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblObjName);
+            this.Controls.Add(this.lblInvNum);
+            this.Controls.Add(this.lblNum);
+            this.Name = "HomePage";
+            this.Text = "Inventory Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Label lblInvNum;
+        private System.Windows.Forms.Label lblObjName;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblNothing;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
